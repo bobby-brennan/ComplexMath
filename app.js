8977e771ec0fc08598b32dc75610941957cb6367
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-25 13:44:19 -0500

    add grid transitions

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ $(document).ready(() => {
 const WIDTH = 600;
 const HEIGHT = 600;
 
-let data = [[1, 1, 50, 50]];
-
-const X_MIN = -1;
-const X_MAX = 1;
+const X_MIN = -3;
+const X_MAX = 3;
 const Y_MIN = X_MIN;
 const Y_MAX = X_MAX;
 const X_SIZE = X_MAX - X_MIN;
@@ -17,17 +15,14 @@ const Y_SIZE = Y_MAX - Y_MIN;
 
 // Need to cast [X_MIN, X_MAX] to [0, WIDTH]
 // First move to [0, X_SIZE], then scale to [0, WIDTH]
-
 const getXCoord = x => {
   let xPixel = x - X_MIN;
   return xPixel * WIDTH / X_SIZE;
 }
-
 const getYCoord = y => {
   let yPixel = y - Y_MIN;
   return HEIGHT - yPixel * HEIGHT / Y_SIZE;
 }
-
 const getPixel = (x, y) => {
   return [getXCoord(x), getYCoord(y)];
 }
@@ -41,7 +36,7 @@ const setUp = () => {
 
   [X_AXIS, Y_AXIS].forEach(axisPoints => {
     let className = '.' + (axisPoints === X_AXIS ? 'x' : 'y') + '-axis';
-    let axis = this.svg.selectAll(className).data([axisPoints]);
+    let axis = window.svg.selectAll(className).data([axisPoints]);
     axis.enter()
         .append('line')
         .attr('class', 'axis')
@@ -52,71 +47,6 @@ const setUp = () => {
         .attr("x2", d => getXCoord(d[1][0]))
         .attr("y2", d => getYCoord(d[1][1]))
   })
-}
-
-class Complex {
-  constructor(x, y, color='steelblue') {
-    this.x = x || 0;
-    this.y = y || 0;
-    this.color = color;
-  }
-
-  copy() {
-    return new Complex(this.x, this.y, this.color);
-  }
-
-  add(z) {
-    this.x += z.x;
-    this.y += z.y;
-    return this;
-  }
-
-  multiply(z) {
-    // (tx + ty*i)(zx + zy*i)
-    // = tx*zx + tx*zy*i + ty*zx*i - ty*zy
-    // = (tx*zx - ty*zy) + (tx*zy + ty*zx)i
-    let newX = this.x * z.x - this.y * z.y;
-    let newY = this.x * z.y + this.y * z.x;
-    this.x = newX;
-    this.y = newY;
-    return this;
-  }
-
-  pow(n) {
-    if (n === 0) {
-      this.x = 1;
-      this.y = 0;
-    } else {
-      let copy = this.copy();
-      for (let i = 1; i < n; ++i) {
-        this.multiply(copy);
-      }
-    }
-    return this;
-  }
-
-  draw() {
-    if (!this.line) this.line = window.svg.append('line');
-    this.line
-          .attr('class', 'complex')
-          .attr('x1', getXCoord(0))
-          .attr('y1', getYCoord(0))
-          .attr('x2', getXCoord(this.x))
-          .attr('y2', getYCoord(this.y))
-          .attr("stroke", this.color)
-          .attr("stroke-width", 3);
-    return this;
-  }
-
-  undraw() {
-    if (!this.line) return;
-    this.line.remove();
-    this.line = null;
-  }
-
-  toString() {
-    return this.x.toFixed(4) + " " + this.y.toFixed(4) + 'i';
-  }
 }
 
 const factorial = (n) => {
@@ -132,15 +62,9 @@ const addExpTerm = (z, cur, term=0) => {
 }
 
 const drawStuff = () => {
-  let i = new Complex(0, 1);
-  let z = new Complex(0, Math.PI);
-  z.draw();
-  let exp = null;
-  let term = 0;
-  setInterval(() => {
-    if (term > 15) return;
-    exp = addExpTerm(z, exp, term++);
-    console.log(exp.toString());
-    exp.draw();
-  }, 500)
+  let w = new Complex(.5, .5);
+  let grid = new Grid(X_SIZE / 30);
+  setTimeout(() => {
+    grid.operate(z => z.multiply(z), 2000);
+  }, 1000);
 }
---
+++ complex.js
@@ -0,0 +1,109 @@
+class Complex {
+  constructor(x, y, color='steelblue') {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.color = color;
+  }
+
+  copy() {
+    return new Complex(this.x, this.y, this.color);
+  }
+
+  getRed() {
+    return d3.scale.linear().domain([X_MIN, X_MAX]).range([0, 255])(this.x);
+  }
+
+  getGreen() {return 0}
+
+  getBlue() {
+    return d3.scale.linear().domain([Y_MIN, Y_MAX]).range([0, 255])(this.y);
+  }
+
+  add(z) {
+    this.x += z.x;
+    this.y += z.y;
+    return this;
+  }
+
+  multiply(z) {
+    // (tx + ty*i)(zx + zy*i)
+    // = tx*zx + tx*zy*i + ty*zx*i - ty*zy
+    // = (tx*zx - ty*zy) + (tx*zy + ty*zx)i
+    let newX = this.x * z.x - this.y * z.y;
+    let newY = this.x * z.y + this.y * z.x;
+    this.x = newX;
+    this.y = newY;
+    return this;
+  }
+
+  pow(n) {
+    if (n === 0) {
+      this.x = 1;
+      this.y = 0;
+    } else {
+      let copy = this.copy();
+      for (let i = 1; i < n; ++i) {
+        this.multiply(copy);
+      }
+    }
+    return this;
+  }
+
+  draw() {
+    if (!this.line) this.line = window.svg.append('line');
+    this.line
+          .transition()
+          .duration(500)
+          .attr('class', 'complex')
+          .attr('x1', getXCoord(0))
+          .attr('y1', getYCoord(0))
+          .attr('x2', getXCoord(this.x))
+          .attr('y2', getYCoord(this.y))
+          .attr("stroke", this.color)
+          .attr("stroke-width", 3);
+    return this;
+  }
+
+  undraw() {
+    if (!this.line) return;
+    this.line.remove();
+    this.line = null;
+  }
+
+  toString() {
+    return this.x.toFixed(4) + " " + this.y.toFixed(4) + 'i';
+  }
+}
+
+class Grid {
+  constructor(stepSize) {
+    let xStep = X_MIN;
+    let yStep = Y_MIN;
+    this.points = [];
+    while (xStep <= X_MAX) {
+      while (yStep <= Y_MAX) {
+        this.points.push(new Complex(xStep, yStep));
+        yStep += stepSize;
+      }
+      yStep = Y_MIN;
+      xStep += stepSize;
+    }
+    this.circles = window.svg.selectAll('.grid-points').data(this.points).enter().append('circle');
+    this.circles
+        .attr('fill', d => d3.rgb(d.getRed(), d.getGreen(), d.getBlue()))
+    this.draw();
+  }
+
+  draw(duration) {
+    this.circles
+        .transition().duration(duration)
+        .attr('cx', d => getXCoord(d.x))
+        .attr('cy', d => getYCoord(d.y))
+        .attr('r', 5)
+  }
+
+  operate(fn, duration=1000) {
+    this.points.forEach(fn);
+    this.draw(duration);
+  }
+}
